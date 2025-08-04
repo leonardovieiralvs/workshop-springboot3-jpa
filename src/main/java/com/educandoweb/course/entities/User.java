8fca36a91006b0b36fb81321520cc02d27034423
leonardovieiralvs <leonardo_vieira199@hotmail.com>
2025-08-04 20:35:12 -0300

    User entity and resource

---
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -0,0 +1,79 @@
+package com.educandoweb.course.entities;
+
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+    @Id
+    private long Id;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+
+    public User() {
+    }
+
+    public User(long id, String name, String email, String phone, String password) {
+        Id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Id == user.Id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(Id);
+    }
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
